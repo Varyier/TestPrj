a64b90076d62abcee263db53804eee9575027fc8
Anisimov V. Andrey <Attervip@yandex.ru>
2014-10-19 13:10:20 +0300

    Added empty function of sorting the array.

--- AA5_Sort.h
+++ AA5_Sort.h
@@ -7,4 +7,11 @@
  #ifndef _AA5_SORT_H_
  #define _AA5_SORT_H_
  
+ /* Sorting an integer array function 
+  * Parameters:
+  * 'arr' - pointer to first element of the array;
+  * 'cnt' - array elements count.
+  */
+ void AA5_Sort( int *arr, int cnt );
+ 
  #endif
---
+++ Res/Sort.c
@@ -0,0 +1,13 @@
+/* File name: SORT.C
+ * Purpose: Sorting implementation.
+ *          For integer arrays.
+ * Programmer: Anisimov V. Andrey
+ */
+
+#include "../AA5_Sort.h"
+
+void AA5_Sort( int *arr, int cnt )
+{
+  if( arr == NULL || cnt < 2 )
+    return;
+}
